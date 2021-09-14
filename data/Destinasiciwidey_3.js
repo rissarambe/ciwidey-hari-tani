59fbf03b9bb771fc32ce9282ae971dee54fe3a78
Rissa Rambe <71384953+rissarambe@users.noreply.github.com>
2021-09-14 16:20:39 +0700

    Add files via upload

---
+++ data/Destinasiciwidey_3.js
@@ -0,0 +1,13 @@
+var json_Destinasiciwidey_3 = {
+"type": "FeatureCollection",
+"name": "Destinasiciwidey_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Agrowisata Teh Rancabali", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.3823906, -7.1470304 ] } },
+{ "type": "Feature", "properties": { "Name": "Danau Kawah Putih", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.404071, -7.1667182 ] } },
+{ "type": "Feature", "properties": { "Name": "Warung Kopi Gunung, Jl. Terusan patengan, Patengan, Bandung, West Java", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.3915112, -7.1442613 ] } },
+{ "type": "Feature", "properties": { "Name": "Kawasan eMTe Resort", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.3976335, -7.1404755 ] } },
+{ "type": "Feature", "properties": { "Name": "Kampung Cai Rancaupas", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.3921829, -7.1382361 ] } },
+{ "type": "Feature", "properties": { "Name": "Bambooberry", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.4267955, -7.1217549 ] } }
+]
+}
---
+++ data/Fasilitasciwidey_2.js
@@ -0,0 +1,12 @@
+var json_Fasilitasciwidey_2 = {
+"type": "FeatureCollection",
+"name": "Fasilitasciwidey_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "RS Max I Salhuteru", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.3415373, -7.1449107 ] } },
+{ "type": "Feature", "properties": { "Name": "Puskesmas Rancabali", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.4194653, -7.1315011 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Ciwidey", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.4647829, -7.103011 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Alam Endah", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.4276, -7.1222399 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Al Hikmah", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.393185, -7.142623 ] } }
+]
+}
